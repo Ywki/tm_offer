337ecc596808d637ae3155750b843db0311752a7
Ywki <uc_supernova@yahoo.co.jp>
2019-11-11 15:50:46 +0900

    first tm

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module TmOffer
+  class Application < Rails::Application
+    config.generators do |g|
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
